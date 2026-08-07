5dfa184f63071b9b05e5a375cefe990f10773b13
amateogr <263436680+amateogr@users.noreply.github.com>
2026-08-07 18:28:05 -0400

    Subiendo nuevo lote de 10 códigos

---
+++ 11070.sql
@@ -0,0 +1,7 @@
+SELECT s.product_id, s.year AS first_year, s.quantity, s.price
+FROM Sales s
+JOIN (
+    SELECT product_id, MIN(year) AS min_year
+    FROM Sales
+    GROUP BY product_id
+) m ON s.product_id = m.product_id AND s.year = m.min_year;
